21d18c7a6cc541c2530b8404c7c1b4f104b51a23
tahsin-roza <tahsinroza10@gmail.com>
2026-06-10 23:52:01 +0600

    form created

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -10,11 +10,10 @@ const NewsLetterBox = () => {
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
         <p className='mt-3 text-gray-400'>Join our mailing list to get updates to your email inbox.</p>
-        <div className='flex justify-center gap-2'>
-            <input className='w-full px-4 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400' type='email' placeholder='Enter your email' />
+        <form onSubmit={onSubmitHandler} className='flex items-center justify-center w-full gap-3 gap-4 mt-5 sm:w-1/2 sm:flex-row'>
+            <input className='w-full outline-none sm:flex-1'type="email" placeholder='Enter your email' />
             <button className='px-6 py-2 text-white transition duration-300 bg-gray-800 rounded-md hover:bg-gray-700'>Subscribe</button>
-        </div>
-      
+        </form>
     </div>
   )
 }

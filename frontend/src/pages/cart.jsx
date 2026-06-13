fef0e32534c04f3240376ed9f18273edb771ce0d
tahsin-roza <tahsinroza10@gmail.com>
2026-06-13 23:22:48 +0600

    added products to admin panel

--- frontend/src/pages/cart.jsx
+++ frontend/src/pages/cart.jsx
@@ -33,10 +33,11 @@ const Cart = () => {
       <div>
         {cartData.map((item, index) => {
           const productData = products.find((product) => product._id === item._id)
+          if (!productData) return null
           return (
             <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
               <div className='flex items-start gap-6'>
-                <img className='w-16 sm:w-20' src={productData.image[0]} alt='' />
+                <img className='w-16 sm:w-20' src={productData.image[0] || 'https://placehold.co/100x100?text=Item'} alt='' />
                 <div>
                   <p className='text-xs font-medium sm:text-lg'>{productData.name}</p>
                   <div className='flex items-center gap-5 mt-2'>
